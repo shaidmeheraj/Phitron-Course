1b52e0a488a4995eb917efd4e867fef53759474c
shaidmeheraj <163715422+shaidmeheraj@users.noreply.github.com>
2024-09-07 16:53:49 +0600

    Add files via upload

---
+++ Algorithm/Basic Graph/STL pair.cpp
@@ -0,0 +1,30 @@
+/* Bismillahir Rahmanir Raheem */
+/* Shaid Meheraj */
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define endl '\n'
+void solve(void)
+{
+    pair<string, int>p;
+    p = make_pair("Meheraj", 2102057);
+    cout << p.first << " " << p.second << endl;
+}
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t=1;
+    while(t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
+
+//Alhamdulillah...
+
+
+
+
